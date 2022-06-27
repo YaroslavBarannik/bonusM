091821fb6d6f5352deb6aa08aef304b61e5d7b1a
YaroslavBarannik <barannik.ya@gmail.com>
2022-06-27 19:34:47 +0300

    <two>

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_000;
+        int miles = price / 20;
+        System.out.println("Дополнительные бонусные милли: " + miles);
+    }
+}
